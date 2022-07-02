9338a5bed608f878b2f145f56fdc3db98bf250b6
SANJAY <nsanjaysanju676@gmail.com>
2022-07-02 18:53:56 +0530

    initial commit 1

---
+++ amazona/frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Channapatna Emporium</a>
+      </header>
+      <main>List products</main>
+    </div>
+  );
+}
+
+export default App;
